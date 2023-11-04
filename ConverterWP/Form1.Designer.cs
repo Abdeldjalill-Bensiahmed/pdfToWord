0be1322a4e06f7ae671fb518efdee9be169a4bc8
Abdeldjalil Bensiahmed <84228068+Abdeldjalill-Bensiahmed@users.noreply.github.com>
2023-11-04 08:46:51 +0100

    Add files via upload

---
+++ ConverterWP/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ConverterWP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Appearance.BackColor = System.Drawing.Color.BlueViolet;
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tajawal ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.Options.UseBackColor = true;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(58, 299);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.simpleButton1.Size = new System.Drawing.Size(294, 55);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "اضغط لاختيار الملف ";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(53, 465);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "المبرمج عبد الجليل بن سي أحمد";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(58, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.MediumPurple;
+            this.Appearance.Options.UseBackColor = true;
+            this.Appearance.Options.UseFont = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(406, 528);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.simpleButton1);
+            this.Font = new System.Drawing.Font("Tajawal ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IconOptions.Image = ((System.Drawing.Image)(resources.GetObject("Form1.IconOptions.Image")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "برنامج تحويل ملفات وورد الى بي دي اف";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ConverterWP/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+
+namespace ConverterWP
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            // Sélectionner le fichier Word à convertir
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Documents Word|*.doc;*.docx";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string wordFilePath = openFileDialog.FileName;
+
+                // Créer une nouvelle instance de l'application Microsoft Word
+                Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+
+                // Ouvrir le document Word
+                Document wordDoc = wordApp.Documents.Open(wordFilePath);
+
+                // Créer un SaveFileDialog pour spécifier le chemin de destination du fichier PDF convertir
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Fichiers PDF|*.pdf";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string pdfFilePath = saveFileDialog.FileName;
+
+                    // Enregistrer le document au format PDF
+                    wordDoc.SaveAs2(pdfFilePath, WdSaveFormat.wdFormatPDF);
+
+                    // Fermer le document Word et l'application
+                    wordDoc.Close();
+                    wordApp.Quit();
+
+                    MessageBox.Show("Conversion terminée !");
+                }
+                else
+                {
+                    // L'utilisateur a annulé le SaveFileDialog, donc fermer le document Word et l'application
+                    wordDoc.Close();
+                    wordApp.Quit();
+                }
+            }
+        }
+    }
+}
